e31918395ba8bb6e5bcbf0f33898237afa6567bf
Baljeet221 <55409075+Baljeet221@users.noreply.github.com>
2019-12-18 14:15:21 -0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,36 @@
+import turtle
+
+smiles = turtle.Turtle()
+
+smiles.penup()
+smiles.goto(-105,155)
+smiles.pendown()
+smiles.goto(-45,115)
+
+smiles.penup()
+smiles.goto(-75,75)
+smiles.pendown()
+smiles.circle(10)
+
+smiles.penup()
+smiles.goto(105,155)
+smiles.pendown()
+smiles.goto(45,115)
+
+smiles.penup()
+smiles.goto(75,75)
+smiles.pendown()
+smiles.circle(10)
+
+smiles.penup()
+smiles.goto(0,25)
+smiles.pendown()
+smiles.circle(-100,80)
+
+smiles.penup()
+smiles.setheading(180)
+smiles.goto(0,25)
+smiles.pendown()
+smiles.circle(100,80)
+
+turtle.done()
